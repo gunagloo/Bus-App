c992f021b8f844a5cc35cf6bf4092fa521a8f7d5
unknown <zheng_guangwei@students.edu.sg>
2022-06-08 14:19:23 +0800

    added styling

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus arrival time:</Text>
+      <Text style={styles.arrivalTime}>08:45:22</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Refresh!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +19,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight: "bold",
+    fontSize: 28,
+    marginBottom: 20
+  },
+  arrivalTime: {
+    fontWeight: "bold",
+    fontSize: 52
+  },
+  button: {
+    backgroundColor: "green",
+    padding: 20,
+    marginTop: 20
+  },
+  buttonText: {
+    fontSize: 28,
+    color: "white",
+    fontWeight: "bold",
+  }
 });
